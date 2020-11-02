e0b35ca193a4bf1033ee44cb13ebde9ca66605ee
shubhampandey111 <shubhampandey051197@gmail.com>
2020-11-02 23:44:27 +0530

    Employee wage UC8

---
+++ EmployeeWageUC8.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+WagePerHr=20
+DailyEmpWage=0
+declare -A DailyWage;
+totalMonthWage=0
+Fulltime=1
+Halftime=2
+absent=0
+empHr=0
+totalworkingdays=0
+echo "Day" "DailyWage" "Total Monthly Wage"
+while [[ $totalworkingdays -lt 20 && $totalworkinghours -lt 100 ]]
+do
+empcheck=$(($RANDOM%3))
+case $empcheck in
+$Fulltime)
+totalworkingdays=$(($totalworkingdays+1))
+empHr=8
+totalworkinghours=$(($totalworkinghours+empHr)) ;;
+$Halftime)
+totalworkingdays=$(($totalworkingdays+1))
+empHr=4
+totalworkinghours=$(($totalworkinghours+empHr)) ;;
+*)
+totalworkingdays=$(($totalworkingdays+1))
+empHr=0
+totalworkinghours=$(($totalworkinghours+empHr)) ;;
+esac
+
+DailyEmpWage=$(($empHr*$WagePerHr))
+DailyWage["Day " $totalworkingdays]=$DailyEmpWage
+totalMonthWage=$(($totalMonthWage+$DailyEmpWage))
+echo "$totalworkingdays" "$DailyEmpWage" "$totalMonthWage"
+done
+echo "Montly Wage=" $totalMonthWage
+echo "Total Working Days=" $totalworkingdays
+echo "Total Working Hours=" $totalworkinghours
+
